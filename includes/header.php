5c376293fa7f28b5884a74ec2594bf63a9519a74
ARCADEFG <112001323+ARCADEFG@users.noreply.github.com>
2025-07-23 23:06:58 +0800

    Update header.php
    Update header from previous code from Jay's Code

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Gamers Community</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-<nav>
-    <a href="index.php">Home</a> |
-    <a href="auth/login.php">Login</a> |
-    <a href="auth/register.php">Register</a>
-</nav>
-<hr>
+<?php // includes/header.php ?>
+<header>
+  <div class="header-content">
+    <h1>GAMERS NEXUS</h1>
+    <div class="tagline">! LEVEL UP YOUR GAMMING DISCUSSIONS AND POST IT HERE !</div>
+    <nav>
+      <a href="index.php"><i class="fas fa-home"></i> Home</a>
+      <a href="register.php"><i class="fas fa-user-plus"></i> Register</a>
+      <a href="login.php"><i class="fas fa-sign-in-alt"></i> Login</a>
+    </nav>
+  </div>
+</header>
